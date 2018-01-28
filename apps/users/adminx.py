f1dae7147dfa658394685546b6587ea70ccc050d
yzzxw <88615442@qq.com>
2018-01-29 07:53:52 +0800

    v

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,3 +1,22 @@
-# _*_ coding: uft-8 _*_
+# _*_ coding: utf-8 _*_
 __author__ = '54h50m'
 __date__ = '2018/1/28 22:00'
+
+import xadmin
+
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index','add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index','add_time']
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+
